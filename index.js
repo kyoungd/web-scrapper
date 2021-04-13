4acfc71d50ea9ddc67b737fdfb642c4a37636554
kyoungd <kyoungd@hotmail.com>
2021-04-13 15:49:06 -0700

    first working version

--- index.js
+++ index.js
@@ -2,6 +2,165 @@
 // const puppeteer = require('puppeteer');
 const puppeteer = require('puppeteer');
 const readline = require('readline');
+const dotenv = require("dotenv")
+const axios = require("axios");
+const himalaya = require('himalaya');
+const { exception } = require('console');
+
+dotenv.config()
+
+async function removeElementByClass(className) {
+    let div_selector_to_remove = className;
+    await page.evaluate((sel) => {
+        var elements = document.querySelectorAll(sel);
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].parentNode.removeChild(elements[i]);
+        }
+    }, div_selector_to_remove)
+}
+
+async function clickButton(page, xpath) {
+    try {
+        // xpath = "/html/body/div[11]/div[2]/div[2]";
+        // xpthh = "/html/body/div[10]/div[2]/div[2]";
+        const searchBtn = await page.$x(xpath);
+        searchBtn[0].click();
+    }
+    catch { }
+}
+
+function cleanHtml(data) {
+    return data
+        .replace(/\n/g, "")
+        .replace(/(&)nbsp;/g, "")
+        .replace(/<tr\s.*?>/g, '<tr>')
+        .replace(/<td\s.*?>/g, '<td>')
+        .replace(/<div\s.*?>/g, '<div>')
+        .replace(/<a\s.*?>/g, '<a>')
+        .replace(/<span\s.*?>/g, '<span>')
+        .replace(/<br.*?>/g, '<br />');
+}
+
+function getFinvizData(name, data) {
+    switch (name) {
+        case "share-float":
+            return data[0].children[1].children[9].children[0].children[0].content;
+        case "short-float":
+            return data[0].children[2].children[9].children[0].children[0].content;
+        default:
+            return "Unknown name on getFinvizData()";
+    }
+}
+
+function getFinvizNews(name, data) {
+    switch (name) {
+        case "date":
+            return data.children[0].children[0].content;
+        case "title":
+            return data.children[1].children[0].children[0].children[0].children[0].content;
+        case "source":
+            return data.children[1].children[0].children[1].children[0].children[0].content;
+        case "quality":
+            try {
+                return data.children[1].children[0].children[1].children[2].children[0].content;
+            } catch (e) {
+                return "";
+            }
+        default:
+            return "unknown name in getFinvizNews()";
+    }
+}
+
+function replaceNbsps(str) {
+    var re = new RegExp(String.fromCharCode(160), "g");
+    return str.replace(re, " ");
+}
+
+function xml2Object(data) {
+    const htmlData = replaceNbsps(data);
+    const obj = himalaya.parse(htmlData);
+    return obj;
+}
+
+let pageLoadCount = 0;
+
+async function finviz(page, url) {
+    // await page.goto(url, { waitUntil: 'networkidle2' });
+
+    try {
+        // await page.goto(url);
+        await page.goto(url, { waitUntil: 'networkidle2' });
+
+        // clickButton(page, "/html/body/div[10]/div[2]/div[2]");
+        // clickButton(page, "/html/body/div[11]/div[2]/div[2]");
+        // await removeElementByClass("ic_dimm1519 ic_fade-in1519");
+        const xpath1 = '//*[@id="news-table"]';
+        const [el] = await page.$x(xpath1);
+        const NewsH = await el.getProperty('innerHTML');
+        // const newsT = await el.getProperty('innerText');
+        // const newsText = await newsT.jsonValue();
+        const newsHt = await NewsH.jsonValue();
+        const newsHtml = cleanHtml(newsHt);
+        const newsJson = xml2Object(newsHtml);
+
+        const xpath2 = '/html/body/div[4]/div/table[2]';
+        const [stat] = await page.$x(xpath2);
+        const dataH = await stat.getProperty('innerHTML');
+        const dataHt = await dataH.jsonValue();
+        const dataHtml = cleanHtml(dataHt);
+        const finvizData = await xml2Object(dataHtml);
+
+        const dataText = "share-float: "
+            + getFinvizData("share-float", finvizData)
+            + "  |  short-float: "
+            + getFinvizData("short-float", finvizData);
+
+        console.log(dataText);
+
+        let newsText = [];
+        let lineCount = 0;
+        const newsArray = newsJson[0].children;
+        for (let ix = 0; ix < newsArray.length; ix++) {
+            const data = newsArray[ix];
+            if (data.children.length > 0) {
+                const oneNews = getFinvizNews("date", data)
+                    + ": " + getFinvizNews("title", data)
+                    + " (" + getFinvizNews("source", data) + ") "
+                    + getFinvizNews("quality", data)
+                newsText.push(oneNews);
+                console.log(oneNews);
+                ++lineCount;
+            }
+            if (lineCount > 6)
+                break;
+        }
+
+        const dataset = { newsText, dataText, news: newsHtml, data: dataHtml };
+        return dataset;
+    }
+    catch (e) {
+        console.log('ERROR: ' + e);
+        if (e.message.indexOf("timeout") > 0 && pageLoadCount < 5) {
+            ++pageLoadCount;
+            return (await finviz(page, url));
+        }
+        else
+            throw exception(e);
+    }
+}
+
+function getBenzingaNews(name, data) {
+    switch (name) {
+        case "date":
+            return data.children[1].children[1].children[0].content;
+        case "title":
+            return data.children[0].children[0].children[0].content;
+        case "source":
+            return data.children[1].children[0].content;
+        default:
+            return "unknown name on getBenzingaNews()";
+    }
+}
 
 async function benzinga(page, url) {
     // await page.goto(url, { waitUntil: 'networkidle2' });
@@ -10,59 +169,68 @@ async function benzinga(page, url) {
     xpath = '//*[@id="__next"]/div/div[2]/div/div/div/div[2]/div[1]/div[1]/div[2]';
     const [el] = await page.$x(xpath);
     const NewsH = await el.getProperty('innerHTML');
-    const newsT = await el.getProperty('innerText');
-    const newsText = await newsT.jsonValue();
-    const newsHTML = await NewsH.jsonValue();
+    const newsHt = await NewsH.jsonValue();
+    const newsHtm = cleanHtml(newsHt);
+    const newsHtml = await xml2Object(newsHtm);
 
-    const dataset = { newsText: newsText, news: newsHTML };
+    const newsArray = newsHtml[0].children;
+    let newsText = [];
+    for (let ix = 0; ix < newsArray.length; ix++) {
+        try {
+            const data = newsArray[ix];
+            const oneNews = getBenzingaNews("date", data)
+                + ": " + getBenzingaNews("title", data)
+                + " (" + getBenzingaNews("source", data) + ")";
+            newsText.push(oneNews);
+            console.log(oneNews);
+            if (ix >= 8)
+                break;
+        }
+        catch (e) {
+            console.log("benzinga: " + e);
+        }
+    }
+
+    // const newsT = await el.getProperty('innerText');
+    // const newsText = await newsT.jsonValue();
+
+    const dataset = { newsText: newsText, news: newsHtml };
     return dataset;
 }
 
-async function finviz(page, url) {
-    // await page.goto(url, { waitUntil: 'networkidle2' });
-    await page.goto(url);
+// function printFinvizData(text) {
+//     const data = text.split('\n');
+//     console.log(data[1]);
+//     console.log(data[2]);
+// }
 
+// function printTextNews(text, maxSize) {
+//     const news = text.split('\n');
+//     for (x = 0; x < (news.length > maxSize ? maxSize : news.length); ++x) {
+//         console.log(news[x]);
+//     }
+// }
+
+async function save(url, name, parseType, dataset, email) {
     try {
-        const xpath1 = '//*[@id="news-table"]';
-        const [el] = await page.$x(xpath1);
-        const NewsH = await el.getProperty('innerHTML');
-        const newsT = await el.getProperty('innerText');
-        const newsText = await newsT.jsonValue();
-        const newsHtml = await NewsH.jsonValue();
-
-        const xpath2 = '/html/body/div[4]/div/table[2]';
-        const [stat] = await page.$x(xpath2);
-        const dataH = await stat.getProperty('innerHTML');
-        const dataHtml = await dataH.jsonValue();
-        const dataT = await stat.getProperty('innerText');
-        const dataText = await dataT.jsonValue();
-
-        const dataset = { newsText: newsText, news: newsHtml, dataText, data: dataHtml };
-        return dataset;
+        const dataset1 = {
+            url,
+            name,
+            parseType,
+            dataset,
+            email,
+        }
+        console.log(JSON.stringify(dataset1, null, 4));
+        await axios.post(process.env.URL, dataset1);
     }
-    catch (e) {
-        console.log(e);
-    }
-}
-
-function printFinvizData(text) {
-    const data = text.split('\n');
-    console.log(data[1]);
-    console.log(data[2]);
-}
-
-function printTextNews(text, maxSize) {
-    const news = text.split('\n');
-    for (x = 0; x < (news.length > maxSize ? maxSize : news.length); ++x) {
-        console.log(news[x]);
-    }
+    catch { }
 }
 
 async function main(symbols) {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     console.log(symbols);
-    for (ix = 2; ix < symbols.length; ++ix) {
+    for (let ix = 2; ix < symbols.length; ++ix) {
         const symbol = symbols[ix];
         console.log('');
         console.log('');
@@ -70,14 +238,22 @@ async function main(symbols) {
         console.log('');
         const url1 = `https://finviz.com/quote.ashx?t=${symbol}`;
         const url2 = `https://www.benzinga.com/quote/${symbol}`;
-        const result1 = await finviz(page, url1);
-        const result2 = await benzinga(page, url2);
-        printFinvizData(result1.dataText);
-        console.log('');
-        printTextNews(result1.newsText, 12);
-        console.log('');
-        printTextNews(result2.newsText, 8);
-        console.log(result2.newsText);
+        try {
+            pageLoadCount = 0;
+            const result1 = await finviz(page, url1);
+            //        await save(url1, "finviz", "finviz", result1, process.env.EMAIL);
+        }
+        catch (e) {
+            console.log("main-finviz: " + e);
+        }
+
+        try {
+            const result2 = await benzinga(page, url2);
+            //        await save(url2, "benzinga", "benzinga", result2, process.env.EMAIL);
+        }
+        catch (e) {
+            console.log("main-benzinga: " + e);
+        }
     }
     browser.close();
 }
